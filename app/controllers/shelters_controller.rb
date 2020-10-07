68012bcf042e6847102b7b6c35a5956bdf53d2d3
Aidan Murray <aidanhansm@gmail.com>
2020-10-06 23:01:36 -0600

    Add create to shelters controller

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -3,11 +3,15 @@ class SheltersController < ApplicationController
     @shelters = Shelter.all
   end
 
-  def new
-
-  end
+  def new; end
 
   def create
-    shelter = Shelter.new()
+    shelter = Shelter.create({
+                               name: params[:shelter][:name],
+                               address: params[:shelter][:address],
+                               city: params[:shelter][:city],
+                               state: params[:shelter][:city],
+                               zip: params[:shelter][:zip]
+                             })
   end
 end
