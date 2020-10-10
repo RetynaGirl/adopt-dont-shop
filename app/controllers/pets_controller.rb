fc1e28aad141cdbbc2413f2383d355f66445a3cb
Aidan Murray <aidanhansm@gmail.com>
2020-10-09 22:11:22 -0600

    Add show path to pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,4 +2,15 @@ class PetsController < ApplicationController
   def index
     @pets = Pet.all
   end
+
+  def show
+    @pet = Pet.find(params[:id])
+    @adoptability = if @pet.adoptable
+                      'Yes'
+                    else
+                      'No'
+                    end
+    @shelter = @pet.shelter
+
+  end
 end
