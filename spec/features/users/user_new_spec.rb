b2bd516e0d6c0c37ad4b71bfba842cc0317b2f70
Will Dunlap <dunlapww@gmail.com>
2020-10-13 15:32:09 -0600

    Create new user test

--- spec/features/users/user_new_spec.rb
+++ spec/features/users/user_new_spec.rb
@@ -19,11 +19,24 @@ describe 'As a visitor' do
     it 'I see a form to create a new user' do
       visit 'users/new'
 
-      expect(page).to have_field(user['name'])
-      expect(page).to have_field(user['address'])
-      expect(page).to have_field(user['city'])
-      expect(page).to have_field(user['state'])
-      expect(page).to have_field(user['zip'])
+      expect(page).to have_field('user[name]')
+      expect(page).to have_field('user[address]')
+      expect(page).to have_field('user[city]')
+      expect(page).to have_field('user[state]')
+      expect(page).to have_field('user[zip]')
+
+    end
+    it "when I fill in form and click 'submit' I'm directed to /user/:id and can see the form data" do
+
+      visit "/users/new"
+      fill_in('user[name]', with: 'Jake')
+      fill_in('user[address]', with: '234 First St.')
+      fill_in('user[city]', with: 'Denver')
+      fill_in('user[state]', with: 'CO')
+      fill_in('user[zip]', with: '80123')
+
+      click_button('Create User')
+      expect(current_path).to eq("/users/#{User.last.id}")
     end
   end
 end
