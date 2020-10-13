7e5f6bc12f2d70d35a4b086d61edf59822342595
Will Dunlap <dunlapww@gmail.com>
2020-10-13 15:32:56 -0600

    Update user controller for new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+
+  end
+
+  def create
+    @user = User.create({
+                     name: params[:user][:name].titleize,
+                     address: params[:user][:address],
+                     city: params[:user][:city],
+                     state: params[:user][:state],
+                     zip: params[:user][:zip]
+                   })
+    redirect_to "/users/#{@user.id}"
+  end
 end
