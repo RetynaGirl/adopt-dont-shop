d040f66a082c6b2f2f34cb42792aa2f8c348ab85
Will Dunlap <dunlapww@gmail.com>
2020-10-14 12:57:59 -0600

    Add spec for show user reviews

---
+++ spec/features/shelter_review/user_reviews_show_spec.rb
@@ -0,0 +1,38 @@
+# User Story 6, User Reviews
+
+# As a visitor
+# When I visit a User's show page
+# Then I see every review this User has written
+# Including the review's title, rating, and content
+
+describe 'As a visitor' do
+  describe 'when I visit a users show page' do
+    before(:each) do
+      @shelter = Shelter.create(name: 'Bobby',
+                               address: '123 halmock st',
+                               city: 'dover',
+                               state: 'florida',
+                               zip: '12345')
+      @user = User.create(name: "Jake",
+                          address: '1234 1st St',
+                          city: 'Denver',
+                          state: 'CO',
+                          zip: '80213') 
+      @review = Review.create(title:"Great shelter",
+                              rating: 4,
+                              content: "They treat their pets well.",
+                              image: 'https://images.dog.ceo/breeds/hound-walker/n02089867_454.jpg',
+                              user: @user,
+                              shelter: @shelter)
+    end
+    it 'I see every review this user has written including review details' do
+       visit "users/#{@user.id}"
+
+       within("#review-#{@review.id}") do
+        expect(page).to have_content(@review.title)
+        expect(page).to have_content(@review.rating)
+        expect(page).to have_content(@review.content)
+       end
+    end
+  end
+end
