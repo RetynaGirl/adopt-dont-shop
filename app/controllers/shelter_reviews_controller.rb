0e90c8c22ad3241cf22751c1c62ef4dbf52a3d3a
Aidan Murray <aidanhansm@gmail.com>
2020-10-14 18:40:13 -0600

    Add controller methods for updating reviews

--- app/controllers/shelter_reviews_controller.rb
+++ app/controllers/shelter_reviews_controller.rb
@@ -16,4 +16,23 @@ class ShelterReviewsController < ApplicationController
 
     redirect_to "/shelters/#{@review.shelter_id}"
   end
+
+  def edit
+    @review = Review.find(params[:id])
+    @users = User.all
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    @review.update(
+      title: params[:review][:title],
+      rating: params[:review][:rating],
+      content: params[:review][:content],
+      user_id: params[:review][:user_id],
+      shelter_id: params[:review][:shelter_id],
+      image: params[:review][:image]
+    )
+
+    redirect_to "/shelters/#{@review.shelter_id}"
+  end
 end
