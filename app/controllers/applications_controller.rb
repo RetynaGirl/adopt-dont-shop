0ce4d30ed7a7cd97fadec2f58e2a7cf466ffece6
Will Dunlap <dunlapww@gmail.com>
2020-10-17 17:48:04 -0600

    Add action to add pet to application

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,5 +4,18 @@ class ApplicationsController < ApplicationController
     @application = Application.find(params[:id])
     @user = @application.user
     @pets = @application.pets
+    if params[:search]
+      @search = params[:search]
+      @pet_search = Pet.where("lower(name) like ?", "%#{params[:search].downcase}%")
+    end
   end
+
+  def add_pet
+    ApplicationPet.create(
+      application_id: params[:app_id], 
+      pet_id: params[:pet_id]
+    )
+    redirect_to "/applications/#{params[:app_id]}"
+  end
+
 end
