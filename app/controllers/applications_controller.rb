1297ec6b194a25cce3ea20be6d7c96f8a868b515
Will Dunlap <dunlapww@gmail.com>
2020-10-19 14:48:35 -0600

    Minor fixes to improve MVC

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,11 +2,12 @@ class ApplicationsController < ApplicationController
 
   def show
     @application = Application.find(params[:id])
-    @user = @application.user
-    @pets = @application.pets
+
     if params[:search]
       @search = params[:search]
-      @pet_search = Pet.where("lower(name) like ?", "%#{params[:search].downcase}%")
+      pets_on_app = @application.pets.pluck(:pet_id)
+      @pet_search = Pet.where("lower(name) like ? ", "%#{params[:search].downcase}%")
+      #@pet_search = Pet.where("pets.id NOT IN (SELECT pets.id FROM pets JOIN pets ON pets.id = application_pets.pet_id WHERE application_pets.application_id =#{@application.id})").where("lower(name) like ? ", "%#{params[:search].downcase}%")
     end
   end
 
