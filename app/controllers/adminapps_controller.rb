cef2c32bce131b9def9076e9051513abc561fe13
Will Dunlap <dunlapww@gmail.com>
2020-10-20 14:31:11 -0600

    remove local variable application from adminapps controller

--- app/controllers/adminapps_controller.rb
+++ app/controllers/adminapps_controller.rb
@@ -5,14 +5,13 @@ class AdminappsController < ApplicationController
 
   def pet_update
     @app_pet = ApplicationPet.where('pet_id = ? AND application_id = ?', params[:pet_id], params[:app_id]).first
-    application = Application.find(params[:app_id])
     case params[:todo]
     when 'approve'
       @app_pet.update(status: 'Approved')
-      application.update(status: "Approved") if application.approved?
+      @app_pet.application.update(status: "Approved") if @app_pet.application.approved?
     when 'reject'
       @app_pet.update(status: 'Rejected')
-      application.update(status: "Rejected")
+      @app_pet.application.update(status: "Rejected")
     end
 
     redirect_to("/admin/applications/#{params[:app_id]}")
