93689f100ac686c0b3a90dc94e0ac3875edacabd
Will Dunlap <dunlapww@gmail.com>
2020-10-19 22:24:10 -0600

    change application variable to local variable in adminapps controller update

--- app/controllers/adminapps_controller.rb
+++ app/controllers/adminapps_controller.rb
@@ -5,14 +5,14 @@ class AdminappsController < ApplicationController
 
   def pet_update
     @app_pet = ApplicationPet.where('pet_id = ? AND application_id = ?', params[:pet_id], params[:app_id]).first
-    @application = Application.find(params[:app_id])
+    application = Application.find(params[:app_id])
     case params[:todo]
     when 'approve'
       @app_pet.update(status: 'Approved')
-      @application.update(status: "Approved") if @application.all_pets_approved
+      application.update(status: "Approved") if application.all_pets_approved
     when 'reject'
       @app_pet.update(status: 'Rejected')
-      @application.update(status: "Rejected")
+      application.update(status: "Rejected")
     end
 
     redirect_to("/admin/applications/#{params[:app_id]}")
